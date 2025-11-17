7afd61fcfdeba3c6159f5b89b70410898eef4ad0
agustin <ruizsosaagustin@institutvidreres.cat>
2025-11-17 21:16:58 +0100

    E11

--- NF3.pt2.js
+++ NF3.pt2.js
@@ -93,16 +93,25 @@
 // console.log(retornaTrue(enter1,enter2))
 
 //10
+//
+// valor1 = 10
+// valor2 = 10
+//
+// var iguals = (v1,v2) => {
+//     if (v1 === v2 && typeof v1 === typeof v2){
+//         return true
+//     }else {
+//         return false
+//     }
+// }
+//
+// console.log(iguals(valor1,valor2))
 
-valor1 = 10
-valor2 = 10
+//11
+arrel = 110
 
-var iguals = (v1,v2) => {
-    if (v1 === v2 && typeof v1 === typeof v2){
-        return true
-    }else {
-        return false
-    }
+var ferArrel =  (v1)=>{
+    var resultatArrel = Math.sqrt(v1)
+    return resultatArrel.toFixed(2)
 }
-
-console.log(iguals(valor1,valor2))
+console.log(ferArrel(arrel))
